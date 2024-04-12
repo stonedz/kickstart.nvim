4d0068e04bef27bc612a197c9ca4d6da94d0a2fc
Paolo Fagni <paolo.fagni@gmail.com>
2024-04-12 16:34:26 +0200

    added copilot, go , rust, harpoon

---
+++ lua/custom/plugins/vim-test.lua
@@ -0,0 +1,23 @@
+return {
+  'vim-test/vim-test',
+
+  dependencies = {
+    'preservim/vimux',
+  },
+
+  vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>'),
+  vim.keymap.set('n', '<leader>tf', ':TestFile<CR>'),
+  vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>'),
+  vim.keymap.set('n', '<leader>tl', ':TestLast<CR>'),
+
+  setup = function()
+    vim.g['test#strategy'] = 'neovim'
+    vim.g['test#neovim#term_position'] = 'botright'
+    vim.g['test#neovim#term_options'] = {
+      'curwin',
+      'vertical',
+      'botright',
+      'split',
+    }
+  end,
+}
