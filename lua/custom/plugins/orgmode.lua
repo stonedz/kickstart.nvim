cbcdc58c149fade45e6c1918828805fb0ff6f3cf
Paolo Fagni <paolo.fagni@gmail.com>
2024-05-23 23:42:38 +0200

    orgmode

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -9,6 +9,19 @@ return {
       org_default_notes_file = '~/orgfiles/inbox.org',
     }
 
+    require('org-bullets').setup {
+      concealcursor = true, -- If false then when the cursor is on a line underlying characters are visible
+      symbols = {
+        list = '•',
+        headlines = { ' ', '󰺕 ', '✸', '✿' },
+        -- headlines = { "◉", "○", "✸", "✿" },
+        checkboxes = {
+          half = { '', 'OrgTSCheckboxHalfChecked' },
+          done = { '✓', 'OrgDone' },
+          todo = { '×', 'OrgTODO' },
+        },
+      },
+    }
     -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
     -- add `org` to ignore_install
     -- require('nvim-treesitter.configs').setup({
